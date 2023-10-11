2fb321ccb3c9b07f9f0b25b30c4232aea729bd2a
Yogesh Shedge <yogeshshedge@Admins-MacBook-Air.local>
2023-10-11 11:03:32 +0530

    Modified

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const port = 3001
 
 app.get('/', (req, res) => {
   console.log('Hello World Log !')
+  getLog()
   res.send('Welcome Hello World!')
 
 })
 
 app.listen(port, () => {
-    
+  getLog()
   console.log(`2.Example app listening on port ${port}`)
 })
+
+function getLog() {
+  const types = ['object', 'array', 'string', 'integer', 'float', 'boolean']
+  for (const type of types) {
+    console.log(`A JavaScript type is: ${type}`)
+  }
+}
