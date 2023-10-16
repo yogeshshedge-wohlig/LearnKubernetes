93a173a5b8143393b1a66919c68e164c7d0d73d4
Yogesh Shedge <yogeshshedge@Admins-MacBook-Air-2.local>
2023-10-16 12:14:14 +0530

    modified

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const app = express()
 const port = 3001
 
 const obj = {name: "John", age: 30, city: "New York"};
-const myJSON = JSON.stringify(obj);
+const myJSON = obj //JSON.stringify(obj);
 
 app.get('/', (req, res) => {
   console.log('Hello World Log Version 0.0.4 !')
   getLog()
-  res.send('Welcome Hello World 2!')
+  res.send('Welcome Hello World 3!')
 
 })
 
 app.listen(port, () => {
   getLog()
-  console.log(`2.Example app listening on port ${port}`)
+  console.log(`3.Example app listening on port ${port}`)
 })
 
 function getLog() {
   const types = ['object', 'array', 'string', 'integer', 'float', 'boolean']
   for (const type of types) {
-    console.log('JSON Data :: ', myJSON , ` Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is: ${type}`)
+    console.log(` Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is: ${type}`)
   }
 }
