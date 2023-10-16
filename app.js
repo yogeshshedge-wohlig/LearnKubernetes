20d239b49b3e459ad3254a0e87e361c5b04b3a33
Yogesh Shedge <yogeshshedge@Admins-MacBook-Air-2.local>
2023-10-16 16:14:37 +0530

    modified

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ const app = express()
 const port = 3001
 
 const obj = {name: "John", age: 30, city: "New York"};
-const myJSON = obj //JSON.stringify(obj);
+const myJSON = JSON.stringify(obj);
 
 app.get('/', (req, res) => {
   console.log('Hello World Log Version 0.0.4 !')
-  getLog()
-  res.send('Welcome Hello World 3!')
+  const types = ['object', 'array', 'string', 'integer', 'float', 'boolean']
+  var arr = []
+  let data = {}
+  for (const type of types) {
+    console.log( 'myJSON :: ', myJSON ,  ` Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is: ${type}`)
+    arr.push(type)
+  }
+  data.arrData= arr
+  data.myJSON= myJSON
+  res.send(data)
 
 })
 
 app.listen(port, () => {
-  getLog()
+  //getLog()
   console.log(`3.Example app listening on port ${port}`)
 })
 
 function getLog() {
   const types = ['object', 'array', 'string', 'integer', 'float', 'boolean']
   for (const type of types) {
-    console.log(` Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is: ${type}`)
+   // console.log( 'myJSON :: ', myJSON ,  ` Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is: ${type}`)
   }
 }
