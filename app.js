0285c6ca24840cf7d4e7b41d8233f7ae60d2d79c
Yogesh Shedge <yogeshshedge@Admins-MacBook-Air-5.local>
2023-10-19 14:02:35 +0530

    modified

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
   var arr = []
   let data = {}
   for (const type of types) {
-    console.log( 'myJSONData', obj ,  ` Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is| Application Version - 0.0.4 and A JavaScript type is | Application Version - 0.0.4 and A JavaScript type is: ${type}`)
+    console.log( 'myJSONData without Stringify :: ', obj)
     arr.push(type)
   }
   data.arrData= arr
